6de83a7eb7d36b4dc495bfd26dea46890846aa06
Jyuki555 <jyuki.work@gmail.com>
2020-02-23 19:22:45 +0900

    aboutページの作成

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="about">
-    aboutページ
+    <div class="about__items">
+      <img src="../assets/circleicon.png" >
+      <p class="about__text">
+        私たちは東京理科大学葛飾キャンパス周辺を中心に活動を行なっています。
+        後輩一人に先輩がマンツーマンで担当し、するので初心者でも問題なくプログラミングを
+        学習することができます。
+      </p>
+    </div>
   </div>
 </template>
 
@@ -10,6 +17,25 @@ export default {
 }
 </script>
 
-<style lang="sass">
+<style scoped>
+
+#about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.about__items img {
+  width: 200px;
+  margin: 20px 0px;
+  height: 200px;
+}
+
+.about__text {
+  width: 600px;
+  margin: 0 auto;
+  font-size: 30px;
+}
 
 </style>
