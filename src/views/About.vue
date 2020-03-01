563db6e6b30ea2ddd56c6415f63f80552d4aa878
Jyuki555 <jyuki.work@gmail.com>
2020-03-01 23:06:31 +0900

    sassに書き換えた

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="about">
-    <div class="about__items">
+    <div class="about-items">
       <img src="../assets/circleicon.png" >
-      <p class="about__text">
+      <p class="about-text">
         私たちは東京理科大学葛飾キャンパス周辺を中心に活動を行なっています。
         後輩一人に先輩がマンツーマンで担当し、するので初心者でも問題なくプログラミングを
         学習することができます。
@@ -17,25 +17,20 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="sass">
 
-#about {
+#about 
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.about__items img {
-  width: 200px;
-  margin: 20px 0px;
-  height: 200px;
-}
-
-.about__text {
-  width: 600px;
-  margin: 0 auto;
-  font-size: 30px;
-}
+  img 
+    width: 200px;
+    margin: 20px 0px;
+    height: 200px;
+  .about-text
+    width: 300px;
+    margin: 0 auto;
+    font-size: 25px;
 
 </style>
