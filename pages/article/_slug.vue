d495369d0882b170cdd23a4e02f10c660a855f44
shota8888 <asbscs@icloud.com>
2020-03-20 00:32:13 +0900

    HeaderNavの変更

---
+++ pages/article/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+import Contentful from 'src/models/Contentful.js'
+
+export default {
+    created() {
+    this.Contentful.getEntries({
+      content_type: "blogPost",
+      'fields.slug': params.slug
+    }).then((res) => this.articles = res.items)
+  },
+  data() {
+    return {
+      articles: []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
