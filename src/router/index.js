3eaf1d1496b7b7e1416bbf68c73e94e24c8b266f
coco <mrkokipro@icloud.com>
2020-08-16 17:51:47 +0900

    remove unnecessary files

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Init from '../views/Init'
 import Index from '../views/Index'
-import Article from '../views/Article'
-import Portfolio from '../views/Portfolio'
-import Home from '../views/Home'
-import Recruit from '../views/Recruit'
+import Root from '../views/Root'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    component: Init,
+    path: '',
+    component: Index,
     children: [
       {
         path: '',
-        component: Index,
-        children: [
-          {
-            path: '',
-            component: Home
-          },{
-            path: 'article',
-            component: Article
-          },{
-            path: 'portfolio',
-            component: Portfolio
-          },{
-            path: 'recruit',
-            component: Recruit
-          }
-        ]
+        component: Root
       }
     ]
   }
