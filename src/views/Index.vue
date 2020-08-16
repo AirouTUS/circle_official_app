144129c3ab7c978a8196831205ef4fd7647990f5
coco <mrkokipro@icloud.com>
2020-08-16 18:12:02 +0900

    reset

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,14 @@
 <template lang="pug">
-  #index
+  div#index
     header-nav
     router-view
 </template>
 
 <script>
-import HeaderNav from '@/components/HeaderNav'
 export default {
-  components: {HeaderNav}
 }
 </script>
 
-<style lang="sass">
-  #index
-    overflow: hidden
+<style lang="sass" scoped>
+
 </style>
