2f9ae25e56f0aff322b417cec67c89f20c539676
coco <mrkokipro@icloud.com>
2020-08-16 17:43:38 +0900

    remove unnecessary files
    contentful

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/sass/global.sass'
-/*import './assets/sass/common.sass'*/
-import Contentful from './models/Contentful'
 import Buefy from 'buefy'
 import VueScrollTo from 'vue-scrollto'
 import VScrollLock from 'v-scroll-lock'
@@ -19,18 +17,6 @@ Vue.use(VueScrollTo, {
 
 Vue.use(Buefy, {
   defaultIconPack: 'fas'
-})
-
-Vue.use({
-  install: vue => {
-    vue.mixin({
-      data() {
-        return {
-          Contentful
-        }
-      }
-    })
-  }
 })
 
 new Vue({
--- src/models/Contentful.js
+++
@@ -1,6 +0,0 @@
-import * as contentful from 'contentful'
-
-export default contentful.createClient({
-  space: process.env.VUE_APP_CTF_SPACE_ID,
-  accessToken: process.env.VUE_APP_CTF_CDA_ACCESS_TOKEN
-})
